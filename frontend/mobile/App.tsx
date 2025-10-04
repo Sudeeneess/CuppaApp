ec313e977f2aad010969cf9f16f6dc7bcf108e47
WaffleWufff <flejerok@gmail.com>
2025-10-05 01:30:40 +0700

    eshkere

---
+++ frontend/mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "red" }}>
+        <View style={styles.container}>
+          <Text>FAFSA</Text>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#e0f51eff",
+  },
+});
