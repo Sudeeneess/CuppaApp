0d8ddff70462980d5334895758f701feec91fe94
ggkamchek <petyapanteev@gmail.com>
2025-10-05 21:00:53 +0700

    Добавил @Data для сущности User

--- backend/src/main/java/com/cuppa/CuppaApp/entity/User.java
+++ backend/src/main/java/com/cuppa/CuppaApp/entity/User.java
@@ -4,11 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 import lombok.Getter;
 
 @Table(name = "Users")
 @Entity
-@Getter
+@Data
 public class User {
     @Id
     @Column(name = "id")
