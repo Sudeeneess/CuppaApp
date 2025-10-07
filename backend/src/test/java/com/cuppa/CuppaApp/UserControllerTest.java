2c04a1718278497bc60b7c860b1959eef21a4d9e
ggkamchek <petyapanteev@gmail.com>
2025-10-07 15:22:08 +0700

    сделал полностью и правильно рабочую бд

--- backend/src/test/java/com/cuppa/CuppaApp/UserControllerTest.java
+++ backend/src/test/java/com/cuppa/CuppaApp/UserControllerTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -45,7 +45,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @version 1.0
  * @since 05.10.2025
  */
-@Sql
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class UserControllerTest {
