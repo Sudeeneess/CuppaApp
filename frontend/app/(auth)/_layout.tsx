16a9885f542cae7e5edf1c2e2fe43e56d91afe13
WaffleWufff <flejerok@gmail.com>
2025-11-03 09:31:41 +0700

    Описал константы светлой и белой тем цветами дизайна, создал хук и использовал его для тематизации приложения

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -1,10 +1,5 @@
 import { Stack } from "expo-router";
-import { PaperProvider } from "react-native-paper";
 
 export default function AuthLayout() {
-  return (
-    <PaperProvider>
-      <Stack screenOptions={{}} />
-    </PaperProvider>
-  );
+  return <Stack screenOptions={{}} />;
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,12 +2,15 @@ import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AuthProvider } from "@/contexts/auth-context";
+import { useAppTheme } from "@/hooks/use-app-theme";
 
 export default function RootLayout() {
+  const theme = useAppTheme();
+
   return (
     <AuthProvider>
       <SafeAreaProvider>
-        <PaperProvider>
+        <PaperProvider theme={theme}>
           <Stack screenOptions={{ headerShown: false }} />
         </PaperProvider>
       </SafeAreaProvider>
